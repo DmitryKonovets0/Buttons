e6e751cc479ab5f0b23b8d4846f46880255051b5
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-02-07 13:04:37 +0200

    finish initialization project

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass')(require('sass'));
+
+function browserSyncFunction (done) {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        },
+        port: 3001
+    });
+    done();
 }
 
-exports.default = defaultTask
+function serv(done) {
+    gulp.watch("./src/scss/*.scss", buildStyles);
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
+    done();
+}
+
+function buildStyles() {
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./src/css'))
+        .pipe(browserSync.stream());
+}
+
+gulp.task('default', gulp.series(browserSyncFunction, gulp.parallel(serv, buildStyles)));
